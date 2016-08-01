0400a8da95a59c45ee883048840cdfbdab9fc074
Dan Daggett <dan@socialgeeks.com>
2016-08-01 16:18:57 +0000

    Add alternative getallheaders() function.  This function no longer exists since the Docker base image changed to Alpine linux.

--- web/index.php
+++ web/index.php
@@ -5,39 +5,58 @@
 </head>
 <body>
     <?php
-	$user = false;
-	$pass = false;
-	$headers =  getallheaders();
-	foreach($headers as $key=>$val){
-	    if($key == 'X-Username'){
-		$user = true;
-		$userval = $val;
-	    }
-	    if($key == 'X-Password'){
-		$pass = true;
-		$passval = $val;
-	    }
-	}  
-	 
+    // http://php.net/manual/en/function.getallheaders.php#84262
+    if (!function_exists('getallheaders'))
+    {
+        function getallheaders()
+        {
+            $headers = '';
+            foreach ($_SERVER as $name => $value)
+            {
+                if (substr($name, 0, 5) == 'HTTP_')
+                {
+                    $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+                }
+            }
+            return $headers;
+        }
+    }
 
-	if($pass && $user && strtoLower($userval) == "admin" && strtoLower($passval) == "password"){
-	    echo("<h4> Welcome Admin</h4>");
-	    echo("<p>KEY: Headers_galor</p>");
-	    echo("<p>WORD: Village</p>");
-	}else{
-	    echo("<h4>Sorry, only Admins can see the key</h4>");
-	    echo("<p>KEY: *************</p>");
-	    if(!$user){
-		echo("<p><b>Error:</b> No Header `HTTP_X-Username` found</p>");
-	    }
-	    if(!$pass){
-		echo("<p><b>Error:</b> No Header `HTTP_X_Password` found</p>");
-	    }
-	}
-	echo('<br><br><h3>Request variables:<br></h3>');
-	print_r($_SERVER);
-	
+    $user = false;
+    $pass = false;
+    $headers =  getallheaders();
+    foreach($headers as $key=>$val){
+        if($key == 'X-Username'){
+            $user = true;
+            $userval = $val;
+        }
+        if($key == 'X-Password'){
+            $pass = true;
+            $passval = $val;
+        }
+    }
+
+
+    if($pass && $user && strtoLower($userval) == "admin" && strtoLower($passval) == "password"){
+        echo("<h4> Welcome Admin</h4>");
+        echo("<p>KEY: Headers_galor</p>");
+        echo("<p>WORD: Village</p>");
+    }else{
+        echo("<h4>Sorry, only Admins can see the key</h4>");
+        echo("<p>KEY: *************</p>");
+        if(!$user){
+            echo("<p><b>Error:</b> No HTTP Header `X-Username` found</p>");
+        }
+        if(!$pass){
+            echo("<p><b>Error:</b> No HTTP Header `X-Password` found</p>");
+        }
+    }
+    echo('<br><br><h3>Request variables:<br></h3>');
+    echo('<pre>');
+    print_r($_SERVER);
+    echo('</pre>');
     ?>
+
     <a href="?password=Password"style="bottom:0; right:0; position:fixed; font-size:3px;">Forgot Your Password?</a>
 </body>
 </html>
